c232030a4dafca14a83fe20f1fafb251e6650c6c
Lukmanul Hakim <jurpro.online@gmail.com>
2022-09-14 07:25:13 +0700

    Half way Application
    Half way Application

---
+++ app/generated/source/buildConfig/debug/com/ujanglukmanbdg/pemeta/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ujanglukmanbdg.pemeta;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.ujanglukmanbdg.pemeta";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  public static final String MAPS_API_KEY = "AIzaSyAzAdHnhSf8GduEBkvGkepWCVzGAa5VEVU";
+}
